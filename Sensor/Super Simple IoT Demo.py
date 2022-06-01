efc73b98abbc0586775de5e03c2756ad1398137a
furlong46 <furlong46>
2022-06-01 13:11:12 +0000

    Commit

--- Sensor/Super Simple IoT Demo.py
+++ Sensor/Super Simple IoT Demo.py
@@ -21,11 +21,28 @@ dbutils.fs.rm("abfss://data@mcg2stanstor00.dfs.core.windows.net/cp/sensorStreamS
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC Basic Stream (100K Devices x 5 Sensors)
+# MAGIC %md 
+# MAGIC ###High Volume IoT Data Processing, ML, Analytics, and Monitoring
+# MAGIC </br>
+# MAGIC <img src="https://mcg1stanstor00.blob.core.windows.net/images/demos/SimpleIoT/overall.jpg" alt="Data Flow" width="725">
+# MAGIC </br></br>
+# MAGIC This demo will walk through an end-to-end data flow ingesting device data in streaming 100K-row batches </br>
+# MAGIC and then joining, transforming, predicting, and ultimately serving that data up to SQL analytics and a </br>
+# MAGIC Power BI monitoring dashboard.  The end-to-end data flow will have insights flowing into Power BI within </br>
+# MAGIC ~10 seconds of cloud ingestion.
 
 # COMMAND ----------
 
+# MAGIC %md 
+# MAGIC ###Device Ingest to Delta Lake using Databricks Autoloader
+# MAGIC </br>
+# MAGIC <img src="https://mcg1stanstor00.blob.core.windows.net/images/demos/SimpleIoT/bronze.jpg" alt="Data Flow" width="400">
+# MAGIC </br></br>
+# MAGIC Simple incremental ingestion from Azure Data Lake or IoT/Event Hubs at ~100K/second.
+
+# COMMAND ----------
+
+# Autoloader Configuration
 df = (
   spark.readStream.format("cloudFiles")
     .option("cloudFiles.format", "json")
@@ -40,6 +57,7 @@ df.createOrReplaceTempView('rawStream')
 # COMMAND ----------
 
 # MAGIC %sql
+# MAGIC -- Query one of the devices from the stream
 # MAGIC SELECT * FROM rawStream
 # MAGIC WHERE device = 87235
 
@@ -47,6 +65,7 @@ df.createOrReplaceTempView('rawStream')
 
 spark.sparkContext.setLocalProperty("spark.scheduler.pool", "pool1")
 
+# Write the device data to the Bronze Delta Lake table
 (
   df.writeStream
     .queryName('sensorStreamBronze')
@@ -61,7 +80,17 @@ spark.sparkContext.setLocalProperty("spark.scheduler.pool", "pool1")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Build a Silver table with Some Joins and a Prediction
+# MAGIC ###Loading a Silver Delta Lake table with Incremental Lookups, Joins, and ML Inference
+# MAGIC </br>
+# MAGIC <img src="https://mcg1stanstor00.blob.core.windows.net/images/demos/SimpleIoT/silver.jpg" alt="Data Flow" width="400">
+# MAGIC </br></br>
+# MAGIC Incremental device data is pulled out of the Bronze Delta table and then:
+# MAGIC 
+# MAGIC 1. it's <b>joined to 2 additional tables</b> to pull reference data
+# MAGIC 2. it has a <b>few basic ETL transformations</b> applied
+# MAGIC 3. an <b>MLflow model is applied</b> to predict what the Sensor5 value should be
+# MAGIC 
+# MAGIC All before being written to a Silver Delta Lake table.
 
 # COMMAND ----------
 
